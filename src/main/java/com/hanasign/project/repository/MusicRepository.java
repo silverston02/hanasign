fa1be39217e278a5f1d4f54296a2b235b70d520a
jeonghana0104 <jeonghan0104@hanyang.ac.kr>
2025-04-29 19:44:53 +0900

    불필요 파일 삭제 - 정하나

--- src/main/java/com/hanasign/project/repository/MusicRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hanasign.project.repository;
-
-import com.hanasign.project.domain.Music;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MusicRepository extends JpaRepository<Music, Long> {
-} 
