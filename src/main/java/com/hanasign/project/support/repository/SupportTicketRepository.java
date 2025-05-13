0b760c3810d648b7d09d4f2f09924818bfef7f38
silverston02 <164297720+silverston02@users.noreply.github.com>
2025-05-13 18:36:25 +0900

    Delete src/main/java/com/hanasign/project/support directory

--- src/main/java/com/hanasign/project/support/repository/SupportTicketRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hanasign.project.support.repository;
-
-import com.hanasign.project.support.domain.SupportTicket;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SupportTicketRepository extends JpaRepository<SupportTicket, Long> {
-}
