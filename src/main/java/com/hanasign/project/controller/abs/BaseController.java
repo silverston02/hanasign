a023a2500a43043e3699b4ffaf74af0b0a666a2e
leenuu <kangjoon10@gmail.com>
2025-05-24 13:28:57 +0900

    base controller

---
+++ src/main/java/com/hanasign/project/controller/abs/BaseController.java
@@ -0,0 +1,21 @@
+package com.hanasign.project.controller.abs;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BaseController {
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+    protected ResponseEntity<Map<String, Object>> createResponseEntity(HttpStatus status, String message, Object data) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", status.value());
+        response.put("message", message);
+        response.put("data", data);
+        return ResponseEntity.status(status).body(response);
+    }
+
+}
