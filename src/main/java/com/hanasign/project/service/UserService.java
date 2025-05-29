b5b896f5c703668eeefb06c97af93f504c0a3260
우썩 <joj4484@naver.com>
2025-05-29 19:51:33 +0900

    조우석 유저 API 커밋밋미미믹

--- src/main/java/com/hanasign/project/service/UserService.java
+++ src/main/java/com/hanasign/project/service/UserService.java
@@ -1,44 +1,11 @@
-// 5. UserService
 package com.hanasign.project.service;
 
-import com.hanasign.project.dto.LoginRequestDto;
-import com.hanasign.project.dto.UserDto;
-import com.hanasign.project.entity.User;
-import com.hanasign.project.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+import com.hanasign.project.dto.userdto.LoginRequestDto;
+import com.hanasign.project.dto.userdto.UserDto;
+import com.hanasign.project.dto.UserResponseDto;
 
-import java.util.Optional;
-
-@Service
-@RequiredArgsConstructor
-public class
-UserService {
-
-    private final UserRepository userRepository;
-
-    public UserDto register(UserDto dto) {
-        User user = new User(
-                null,
-                dto.getName(),
-                dto.getEmail(),
-                dto.getPw(),
-                dto.getPhonNumber(),
-                null,
-                null,
-                null
-        );
-        return new UserDto(userRepository.save(user));
-    }
-
-    public boolean login(LoginRequestDto dto) {
-        Optional<User> user = userRepository.findByEmailAndPw(dto.getEmail(), dto.getPw());
-        return user.isPresent();
-    }
-
-    public UserDto getUserById(Long id) {
-        User user = userRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다: id=" + id));
-        return new UserDto(user);
-    }
+public interface UserService {
+    UserDto register(UserDto dto);
+    boolean login(LoginRequestDto dto);
+    UserResponseDto getUserById(Long id);
 }
