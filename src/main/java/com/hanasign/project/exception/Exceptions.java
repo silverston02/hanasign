60cedc3a4ab989639906c807b1aa4db26aeb360b
우썩 <joj4484@naver.com>
2025-06-04 20:39:12 +0900

    조우석 유저 API 커밋밋미미믹

--- src/main/java/com/hanasign/project/exception/Exceptions.java
+++ src/main/java/com/hanasign/project/exception/Exceptions.java
@@ -3,6 +3,7 @@ package com.hanasign.project.exception;
 import org.springframework.http.HttpStatus;
 
 public class Exceptions {
-    public static final CustomException USER_NOT_FOUND = new CustomException(HttpStatus.NOT_FOUND, "10001", "사용자를 찾을 수 없습니다.");
+    public static final CustomException USER_NOT_FOUND = new CustomException(HttpStatus.NOT_FOUND, "20001", "사용자를 찾을 수 없습니다.");
 
-}
+
+    }
