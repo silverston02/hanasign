b91ce724c9d0c8cfe89f5d284138914d60f1f15d
ParkSuMin0611 <smpp.park@gmail.com>
2025-06-05 20:01:18 +0900

    -박수민 거래 히스토리 거래 코멘트

--- src/main/java/com/hanasign/project/controller/ContractHistoryController.java
+++ src/main/java/com/hanasign/project/controller/ContractHistoryController.java
@@ -1,7 +1,9 @@
 package com.hanasign.project.controller;
 
 import com.hanasign.project.controller.abs.BaseController;
-import com.hanasign.project.dto.ContractHistoryDto;
+import com.hanasign.project.dto.ContractHistoryDto.ContractHistoryRequestDto;
+import com.hanasign.project.dto.ContractHistoryDto.ContractHistoryResponseDto;
+import com.hanasign.project.exception.CustomException;
 import com.hanasign.project.service.ContractHistoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -19,14 +21,14 @@ public class ContractHistoryController extends BaseController {
     private final ContractHistoryService service;
 
     @PostMapping("/save")
-    public ResponseEntity<Map<String, Object>> saveHistory(@RequestBody ContractHistoryDto dto) {
-        ContractHistoryDto saved = service.save(dto);
+    public ResponseEntity<Map<String, Object>> saveHistory(@RequestBody ContractHistoryRequestDto dto) throws CustomException {
+        ContractHistoryResponseDto saved = service.save(dto);
         return createResponseEntity(HttpStatus.OK, "히스토리 저장 성공", saved);
     }
 
     @GetMapping("/find-all/{contractId}")
-    public ResponseEntity<Map<String, Object>> getHistories(@PathVariable Long contractId) {
-        List<ContractHistoryDto> list = service.getByContractId(contractId);
+    public ResponseEntity<Map<String, Object>> getHistories(@PathVariable Long contractId) throws CustomException {
+        List<ContractHistoryResponseDto> list = service.getByContractId(contractId);
         return createResponseEntity(HttpStatus.OK, "히스토리 조회 성공", list);
     }
 }
