70e942c785a5921a19aa24e29b582eb0f763dccc
silverston02 <es121720@gmail.com>
2025-06-07 13:42:53 +0900

    - 은석 - contract 250607

--- src/main/java/com/hanasign/project/exception/Exceptions.java
+++ src/main/java/com/hanasign/project/exception/Exceptions.java
@@ -4,16 +4,16 @@ import org.springframework.http.HttpStatus;
 
 public class Exceptions {
 
-    public static final CustomException USER_NOT_FOUND = new CustomException(HttpStatus.NOT_FOUND, "20001", "사용자를 찾을 수 없습니다.");
 
     public static final CustomException USER_NOT_FOUND = new CustomException(HttpStatus.NOT_FOUND, "10001", "사용자를 찾을 수 없습니다.");
-
-    public static final CustomException CONTRACT_NOT_FOUND = new CustomException(HttpStatus.NOT_FOUND, "20001", "계약을 찾을 수 없습니다");
-    public static final CustomException SUPPLIER_NOT_FOUND = new CustomException(HttpStatus.NOT_FOUND, "20001", "계약을 찾을 수 없습니다");
-    public static final CustomException CLIENT_NOT_FOUND = new CustomException(HttpStatus.NOT_FOUND, "20001", "계약을 찾을 수 없습니다");
-
     public static final CustomException EXPIRED_JWT = new CustomException(HttpStatus.UNAUTHORIZED, "10003", "JWT 토큰이 만료되었습니다.");
     public static final CustomException INVALID_JWT = new CustomException(HttpStatus.UNAUTHORIZED, "10004", "JWT 토큰이 틀렸습니다.");
+
+    // 계약 관련 EXCEPTIONS
+    public static final CustomException CONTRACT_NOT_FOUND = new CustomException(HttpStatus.NOT_FOUND, "30001", "계약을 찾을 수 없습니다");
+    public static final CustomException SUPPLIER_NOT_FOUND = new CustomException(HttpStatus.NOT_FOUND, "30002", "계약을 찾을 수 없습니다");
+    public static final CustomException CLIENT_NOT_FOUND = new CustomException(HttpStatus.NOT_FOUND, "30003", "계약을 찾을 수 없습니다");
+    public static final CustomException CONTRACT_USER_NOT_FOUND = new CustomException(HttpStatus.BAD_REQUEST, "30004","사용자가 이 계약의 클라이언트나 공급자가 아닙니다.");
 
     public static final CustomException COMPANY_NOT_FOUND = new CustomException(HttpStatus.NOT_FOUND, "40001", "회사를 찾을 수 없습니다.");
 
@@ -24,6 +24,7 @@ public class Exceptions {
 
     public static final CustomException WRONG_EXTENSIONS = new CustomException(HttpStatus.BAD_REQUEST, "31001", "지원하지 않는 파일 형식");
     public static final CustomException FILE_NOT_FOUND = new CustomException(HttpStatus.BAD_REQUEST, "31002", "일치하는 파일이 없습니다. ");
+    public static final CustomException FILE_TRANS_ERROR = new CustomException(HttpStatus.INTERNAL_SERVER_ERROR, "31003", "파일 전송 중 오류가 발생했습니다"); //은석 추가
 
     //  Comment 관련 예외 코드
     public static final CustomException COMMENT_CONTENT_REQUIRED =
